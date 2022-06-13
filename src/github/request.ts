856e629455e4c6e3a7decdb52c13e1e018c1fbbe
rhythm022 <rhythm22@yeah.net>
2022-06-13 21:30:53 +0800

    g14: 支持下载模板时选择分支

---
+++ src/github/request.ts
@@ -0,0 +1,46 @@
+/*
+ * @Author: Cookie
+ * @LastEditors: Cookie
+ * @LastEditTime: 2021-08-13 16:37:30
+ * @Description: request 模块
+ */
+import { loggerError } from '@/util';
+import request from 'request'
+const qs = require("qs");
+
+interface IMethodV {
+  url: string
+  params?: object
+  query?: object
+}
+
+const GET = async <T>({ url, query = {} }: IMethodV) => {
+  const sendUrl = `${url}?${qs.stringify(query)}`;
+  try {
+    return new Promise<T>((resolve, reject) => {
+      request({
+        url: sendUrl,
+        method: "GET",
+        json: true,
+        headers: {
+          "content-type": "application/json",
+          "Accept": "application/vnd.github.v3+json",
+          "User-Agent": "Fe-Cli-App",
+        },
+      }, (error, response, body) => {
+        if (!error && body) {
+          resolve(body)
+        } else {
+          reject(error)
+        }
+      });
+    })
+  } catch (error) {
+    loggerError(error as string)
+    throw (error);
+  }
+}
+
+export {
+  GET,
+}
